9a9271885bfc7ea4d9a648db987e3cb9cb0de488
youn9jo <58538763+youn9jo@users.noreply.github.com>
2019-12-08 15:36:45 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ setInterval(function(){
     var month = calender.getMonth() + 1;
     var date = calender.getDate();
     var week = ['일', '월', '화', '수', '목', '금', '토'];
-    var day = "(" + week[calender.getDay()] "+";
+    var day = "(" + week[calender.getDay()] + ")";
     if( month < 10 ) month = '0' + month;
     if( date < 10 ) date = '0' + date;
     document.getElementById("date").innerHTML = year + "年" + " " + month + "月" + " " + date + "日" + " " + day});
